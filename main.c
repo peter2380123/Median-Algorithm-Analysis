10d815f3d10d4894d59270542a1c6c244d456ffb
Jonathan F <fungjys@gmail.com>
2018-04-16 21:33:36 +1000

    Added reading from command line.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // C++ bindings for the Merseinne Twister.
 #include "mt_random.h"
@@ -11,16 +12,20 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc == 5) // User has explicitly asked for number of trials.
-    {
-    }
-    if (argc <= 4)
-    {
-    }
-    else
+    if (argc < 4 || argc > 5)
     {
         printf("./median start stop numberOfIncrements numberOfTrials\n");
         return -1;
     }
+
+    size_t start = atoi(argv[1]), stop = atoi(argv[2]), numIncrements = atoi(argv[3]), numTrials = 1;
+
+    if (argc == 5)
+    {
+        numTrials = atoi(argv[4]);
+    }
+
+    printf("start: %zu\tstop: %zu\tnumIncrements: %zu\tnumTrials: %zu\n", start, stop, numIncrements, numTrials);
+
     return 0;
 }
