350e2894a4790a2610f15e2376e63f500a3fcb67
Jonathan F <fungjys@gmail.com>
2018-04-16 21:17:06 +1000

    Remove static modifier from uniform_int_distribution.

--- mt_random.cpp
+++ mt_random.cpp
@@ -9,7 +9,7 @@ int mt_random(int inclusiveMin, int inclusiveMax)
 {
     static std::random_device rd;
     static std::mt19937 gen(rd()); // Seed.
-    static std::uniform_int_distribution<> dis(inclusiveMin, inclusiveMax);
+    std::uniform_int_distribution<> dis(inclusiveMin, inclusiveMax);
     
     return dis(gen);
 }
