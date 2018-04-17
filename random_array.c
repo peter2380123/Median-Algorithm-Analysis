7e19625de753d0bfa34330b3b35dbc7900cea9c5
Jonathan F <fungjys@gmail.com>
2018-04-17 21:29:24 +1000

    Change random_array to populate an array rather than create one.

--- random_array.c
+++ random_array.c
@@ -4,14 +4,10 @@
 #include "random_array.h"
 #include "mt_random.h"
 
-int *create_random(const size_t size)
+void populate_random(int *mem, const size_t size)
 {
-    int *result = malloc(size * sizeof(int));
-
     for (size_t i = 0; i < size; ++i)
     {
-        result[i] = mt_random(INT_MIN, INT_MAX);
+        mem[i] = mt_random(INT_MIN, INT_MAX);
     }
-
-    return result;
 }
