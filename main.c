6c89149ce655a6af512e3e6e64fd41ffc284a09c
Jonathan F <fungjys@gmail.com>
2018-05-13 10:29:59 +1000

    Add timer functionality.
    Additionally, change compile options for conditional compilation to
    produce separate binaries for basic operations and time.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 
 // C++ bindings
 #include "mt_random.h" // Merseinne Twister
@@ -13,11 +14,42 @@
 
 #define MAX(x, y) (((x) >= (y)) ? (x) : (y))
 
+#ifdef COUNT_OPS
+const char *brute_result = "BRUTE_OPERATIONS";
+const char *quick_result = "QUICK_OPERATIONS";
+#else
+const char *brute_result = "BRUTE_TIME";
+const char *quick_result = "QUICK_TIME";
+#endif
+
+// A helper function to fill an existing array with random values.
+static void populate_random(int *mem, const size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        mem[i] = mt_random(INT_MIN, INT_MAX);
+    }
+}
+
+// Program entry point.
 int main(int argc, char *argv[])
 {
     size_t start, stop = 100, numIncrements = 1, numTrials = 1;
 
-#ifdef __GNUC__
+#if _MSC_VER
+    /*unsigned int start_value;
+      unsigned int stop_value;
+      int numIncrements;
+      int numTrial;*/
+    printf("Declare inclusive start value for the random array: ");
+    scanf("%zu", &start);
+    printf("Declare inclusive stop value for the random array: ");
+    scanf("%zu", &stop);
+    printf("Number of increments: ");
+    scanf("%zu", &numIncrements);
+    printf("Number of trials to run: ");
+    scanf("%zu", &numTrials);
+#else
     if (argc < 4 || argc > 5)
     {
         printf("./median start stop numberOfIncrements numberOfTrials\n");
@@ -31,46 +63,89 @@ int main(int argc, char *argv[])
     {
         numTrials = atoi(argv[4]);
     }
-#elif _MSC_VER
-	/*unsigned int start_value;
-	unsigned int stop_value;
-	int numIncrements;
-	int numTrial;*/
-	printf("Declare inclusive start value for the random array: ");
-	scanf("%zu", &start);
-	printf("Declare inclusive stop value for the random array: ");
-	scanf("%zu", &stop);
-	printf("Number of increments: ");
-	scanf("%zu", &numIncrements);
-	printf("Number of trials to run: ");
-	scanf("%zu", &numTrials);
-
 #endif
 
     double increment = MAX(1, (stop - (double)start) / MAX(1.0, ((double)numIncrements-1.0)));
 
-    int *data = malloc(stop * sizeof(int));
+    // Allocate a contiguous block of memory the size of our largest N size.
+    // This way, we avoid continuously allocating and freeing memory of various
+    // sizes, which can inconsistently affect the performance due to caching.
+    // We don't initialise to 0 because we are going to overwrite the values
+    // anyway.
+    int *data = malloc(MAX(start, stop) * sizeof(int));
+
+    // As a side-effect, we can now also stop the program from running if the
+    // largest N size is too big for the computer's memory.
     if (data == NULL)
     {
-        printf("Could not allocate memory. Try a smaller end size.\n");
+        printf("Could not allocate memory. Try a smaller N. (You entered %zd)\n", MAX(start, stop));
         return -1;
-    }
-
-    // Placeholder stuff.
-    printf("start: %zu\tstop: %zu\tnumIncrements: %zu\tnumTrials: %zu\n", start, stop, numIncrements, numTrials);
-
-    for (double i_ = start; round(i_) <= stop; i_ += increment)
-    {
-        const size_t i = round(i_); // Do not change.
-        printf("i_: %f\n", i_);
-        printf("i = %zu\n", i);
     }
 
     // Our output file.
     FILE *f = fopen("results.csv", "w");
-    fprintf(f, "INPUT_SIZE, BRUTE_NUM_OPERATIONS, BRUTE_NANOSECONDS, QUICK_NUM_OPERATIONS, QUICK_NANOSECONDS\n");
+    fprintf(f, "INPUT_SIZE, %s, %s\n", brute_result, quick_result);
+
+    // For every N...
+    for (double i = start; round(i) <= stop; i += increment)
+    {
+        const size_t N = round(i);
+
+        // These variables will hold the total measurement.
+        double bfTotal = 0, quickTotal = 0;
+
+        // Trial it numTrials times.
+        for (size_t trial = 1; trial <= numTrials; ++trial)
+        {
+
+            // Give user some feedback to what is happening.
+            printf("Trial %zu of %zu, n = %zu\n", trial, numTrials, N);
+
+            // Populate data with random values, using the standardised
+            // Merseinne Twister implementation from C++. We use this instead of
+            // rand() because the quality of rand() is implementation-defined,
+            // while the quality of C++'s Merseinne Twister is guaranteed. The
+            // trade-off for this is that this program must be compiled on a
+            // machine with both a C and C++ compiler -- which is guaranteed in
+            // this subject as both languages are accepted.
+            populate_random(data, N);
+
+            long long t0, tf;
+            // We're doing a comparison, and therefore should have the functions
+            // both find the median from the same sequence of numbers. Note that
+            // brute force median causes no side-effects, but quick median does.
+            // Therefore, we run brute force median first.
+#ifndef COUNT_OPS // TODO: Switch around when both implemented.
+            t0 = get_monotime();
+            bfm(data, N);
+            tf = get_monotime();
+
+            bfTotal += tf - t0;
+
+            // Now our quick median.
+            t0 = get_monotime();
+            qm_median(data, N);
+            tf = get_monotime();
+
+            quickTotal += tf - t0;
+#else
+            // Code to count basic operations goes here.
+#endif
+        } // end trials for.
+
+        // At this point, we need to take the average to make any sense of our
+        // result. Awkward cast to make sure this is not an integer division.
+        double bfAverage = bfTotal / (double)numTrials;
+        double quickAverage = quickTotal / (double)numTrials;
+
+        // Now we an write this result into the file, appending it to whatever
+        // was there last.
+        fprintf(f, "%zd, %f, %f\n", N, bfAverage, quickAverage);
+
+    } // end N for.
 
     free(data);
+    fclose(f);
 
     return 0;
 
